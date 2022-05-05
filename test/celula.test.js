eef1b692eccfbce569753426465092c5e63fc23d
Deniz-Navarro <ldeniz1@ucol.mx>
2022-05-05 17:23:40 -0500

    EsLint and Prettier implementation

---
+++ test/celula.test.js
@@ -0,0 +1,9 @@
+const Celula = require('./celula');
+
+
+describe("Pruebas de la clase Cell", () => {
+  test("probamos el estado random", () => { });
+  test("probamos el estado random", () => { });
+  test("probamos el estado random", () => { });
+  test("probamos el estado random", () => { });
+});
